6e860e9fcc74df4b4f75c753ab6f57cf35ec85dd
nutmag <60609432+nutmag@users.noreply.github.com>
2020-02-12 12:10:10 +0530

    Update owasp.sh

--- owasp.sh
+++ owasp.sh
@@ -17,7 +17,7 @@ fi
 docker pull owasp/dependency-check
 
 docker run --rm \
-    --volume $(pwd):/src \
+    --volume $(pwd):https://raw.githubusercontent.com/nutmag/Devsecops/master/owasp.sh \
     --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
     --volume "$REPORT_DIRECTORY":/report \
     owasp/dependency-check \
